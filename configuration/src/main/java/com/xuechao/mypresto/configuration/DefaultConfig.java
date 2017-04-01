eedf35060ae6772e36c152162cb60596e773a50c
xuechao <xuechao@153.65.129.23>
2017-04-01 14:58:53 +0800

    dd

---
+++ configuration/src/main/java/com/xuechao/mypresto/configuration/DefaultConfig.java
@@ -0,0 +1,18 @@
+package com.xuechao.mypresto.configuration;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import com.google.inject.BindingAnnotation;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@BindingAnnotation
+public @interface DefaultConfig {
+
+	String[] value();
+}
